9d5ecb82a5cf5fd34a50d9b882450b180a4c21e2
Pbjsd <104098660+Pbjsd@users.noreply.github.com>
2024-04-16 19:20:46 -0400

    Initial Commit

---
+++ FastingTimer/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  FastingTimer
+//
+//  Created by Panchi on 4/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      ZStack {
+        // MARK: Background
+
+        Color(#colorLiteral(red: 0.05024140328, green: 0.006751002744, blue: 0.08163713664, alpha: 1))
+          .ignoresSafeArea()
+
+        content
+        }
+    }
+
+  var content: some View {
+    VStack {
+      // MARK: 
+    }
+  }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FastingTimer/FastingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTimerApp.swift
+//  FastingTimer
+//
+//  Created by Panchi on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FastingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
