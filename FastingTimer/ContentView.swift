088d0762a468e77c4fe10a2c415b7eddf8daf95e
Pbjsd <104098660+Pbjsd@users.noreply.github.com>
2024-04-19 12:36:08 -0400

    Added starting and end time to timer

--- FastingTimer/ContentView.swift
+++ FastingTimer/ContentView.swift
@@ -8,26 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-      ZStack {
-        // MARK: Background
-
-        Color(#colorLiteral(red: 0.05024140328, green: 0.006751002744, blue: 0.08163713664, alpha: 1))
-          .ignoresSafeArea()
-
-        content
-        }
-    }
-
-  var content: some View {
-    VStack {
-      // MARK: Progress Ring
-
-      ProgressRing()
+  var body: some View {
+    ZStack {
+      // MARK: Background
+      
+      Color(#colorLiteral(red: 0.05024140328, green: 0.006751002744, blue: 0.08163713664, alpha: 1))
+        .ignoresSafeArea()
+      
+      content
     }
   }
+  
+  var content: some View {
+    VStack {
+      // MARK: Title
+      
+      Text("Let's get started!")
+        .font(.headline)
+        .foregroundColor(Color(#colorLiteral(red: 0.4605398178, green: 0.5195819736, blue: 1, alpha: 1)))
+      
+      // MARK: Fasting Plan
+      
+      Text("16:8")
+        .fontWeight(.bold)
+        .padding(.horizontal, 24)
+        .padding(.vertical, 8)
+        .background(.thinMaterial)
+        .cornerRadius(20)
+      
+      // MARK: Progress Ring
+      
+      ProgressRing()
+      
+      HStack(spacing: 60) {
+        // MARK: Start Time
+        
+        VStack(spacing: 5) {
+          Text("Start")
+            .opacity(0.7)
+          
+          Text(Date(), format: .dateTime.weekday().hour().minute().second())
+            .fontWeight(.bold)
+        }
+        
+        // MARK: End Time
+        
+        VStack(spacing: 5) {
+          Text("End")
+            .opacity(0.7)
+          
+          Text(Date().addingTimeInterval(16), format: .dateTime.weekday().hour().minute().second())
+            .fontWeight(.bold)
+        }
+      }
+    }
+    .foregroundColor(.white)
+  }
 }
-
 #Preview {
     ContentView()
 }
